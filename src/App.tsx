4ffce4b294869abfd0f06202d5d9b796997f74bc
Promise Femi <promisefemi90@gmail.com>
2024-10-04 13:33:24 +0100

    added brightness check

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {useCallback, useEffect, useRef, useState} from 'react'
 import './App.css'
 import Webcam from "react-webcam";
 import * as faceApi from 'face-api.js'
-import {FaceDetection, FaceLandmarks68, WithFaceDescriptor, WithFaceLandmarks} from "face-api.js";
+import {FaceDetection, FaceLandmarks68, WithFaceDescriptor, WithFaceLandmarks} from 'face-api.js'
 
 
 function App() {
@@ -19,17 +19,47 @@ function App() {
     const capturePhoto = async () => {
         if (webCamRef.current && faceApiReady) {
             const imageUrl = webCamRef.current.getScreenshot();
+          //  console.log(imageUrl);
             setImage(imageUrl)
-            const imageInput = await faceApi.fetchImage(imageUrl)
-            const fileDescriptors = await faceApi.detectAllFaces(imageInput).withFaceLandmarks().withFaceDescriptors();
-            console.log(fileDescriptors);
-            if (fileDescriptors.length > 1) {
-                alert("More than one person in camera frame")
-                return
+            const imageBrightness = checkImageBrightness();
+            console.log(imageBrightness);
+            if (imageBrightness>= 130) {
+                const imageInput = await faceApi.fetchImage(imageUrl)
+                const fileDescriptors = await faceApi.detectAllFaces(imageInput).withFaceLandmarks().withFaceDescriptors();
+                console.log(fileDescriptors);
+                if (fileDescriptors.length > 1) {
+                    alert("More than one person in camera frame")
+                    return
+                }
+                setBaseImageFileDescriptors(fileDescriptors[0])
+            } else {
+                alert("Image is too dark, please go to a well lit environment")
             }
-            setBaseImageFileDescriptors(fileDescriptors[0])
         }
     }
+
+    const checkImageBrightness = () => {
+        if (canvasRef.current && webCamRef.current) {
+            const ctx = canvasRef.current.getContext('2d');
+            ctx.drawImage(webCamRef.current.video, 0, 0, canvasRef.current.width, canvasRef.current.height);
+            const imageData = ctx.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height);
+            const data = imageData.data;
+            let r, g, b, avg;
+            let colorSum = 0
+            for (let x = 0, len = data.length; x < len; x += 4) {
+                r = data[x];
+                g = data[x + 1];
+                b = data[x + 2];
+
+                avg = Math.floor((r + g + b) / 3);
+                colorSum += avg;
+            }
+
+            return Math.floor(colorSum / (canvasRef.current.width * canvasRef.current.height));
+        }
+        return 0;
+    }
+
 
     const testFaceMatch = async () => {
         if (webCamRef.current && faceApiReady && baseImageFileDescriptors) {
@@ -38,6 +68,10 @@ function App() {
             const maxDescriptorDistance = 0.5
             const faceMatcher = new faceApi.FaceMatcher(baseImageFileDescriptors, maxDescriptorDistance)
             const fileDescriptors = await faceApi.detectAllFaces(imageInput).withFaceLandmarks().withFaceDescriptors();
+           if(!fileDescriptors.length){
+               alert("cannot find any face for image, please go to a well lit environment")
+               return
+           }
             if (fileDescriptors.length > 1) {
                 alert("More than one person in camera frame")
                 return
@@ -55,7 +89,7 @@ function App() {
 
     useEffect(() => {
         setInterval(testFaceMatch, 1000)
-    },[testFaceMatch])
+    }, [testFaceMatch])
 
     const initializeFaceApiModels = useCallback(async () => {
 
@@ -92,6 +126,7 @@ function App() {
                         <td>
 
                             <Webcam ref={webCamRef} width={800} height={450}/>
+                            <canvas ref={canvasRef} width={800} height={450} style={{display:"none"}}/>
                             <button onClick={capturePhoto}
                                     disabled={!faceApiReady}>{faceApiReady ? "Take Reference Picture" : "Loading face api model..."}</button>
                             {baseImage &&
